280a3073065d4e67f9f9e1632f5b3e4c9345b86b
Jonathan Hellberg <jonathanh98@live.se>
2019-09-22 19:37:26 +0200

    cross orgin

--- app.js
+++ app.js
@@ -14,7 +14,13 @@ const port = 8333;
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Accept');
+});
 
 // don't show the log when it is test
 if (process.env.NODE_ENV !== 'test') {
