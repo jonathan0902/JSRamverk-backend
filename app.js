228ab954467598c141ba7300387dc91674034b46
Jonathan Hellberg <jonathanh98@live.se>
2019-09-22 19:21:20 +0200

    cross orgin

--- app.js
+++ app.js
@@ -54,10 +54,4 @@ app.use((err, req, res, next) => {
     });
 });
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
 app.listen(port, () => console.log(`Example API listening on port ${port}!`));
